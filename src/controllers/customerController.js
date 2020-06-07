16b0291df4c4132a8896d6007bf53c02a76c17dc
samuel <sami_zar@hotmail.com>
2020-06-07 16:19:14 -0500

    cosmetics

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -14,8 +14,49 @@ controller.list=(req,res)=>{
   });
 };
 
-controller.save=(req,res)=>{
 
+controller.save = (req, res) => {
+  const{name,address,phone} = req.body;
+  console.log("es el save")
+  console.log(req.body)
+  req.getConnection((err, conn) => {
+     conn.query('INSERT INTO customer set ?', {name,address,phone}, (err, cust) => {
+      console.log("cutomes")
+      res.redirect('/');
+    })
+  })
+};
+
+controller.edit = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM customer WHERE id = ?", [id], (err, rows) => {
+      res.render('customers_edit', {
+        data: rows[0]
+      })
+    });
+  });
+};
+
+controller.update = (req, res) => {
+  const { id } = req.params;
+  const newCustomer = req.body;
+  req.getConnection((err, conn) => {
+
+  conn.query('UPDATE customer set ? where id = ?', [newCustomer, id], (err, rows) => {
+    res.redirect('/');
+  });
+  });
+};
+
+controller.delete = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, connection) => {
+    connection.query('DELETE FROM customer WHERE id = ?', [id], (err, rows) => {
+      res.redirect('/');
+    });
+  });
 }
 
-module.exports=controller;
+module.exports = controller;
